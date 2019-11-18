91f955860d5f1e46c91d9ce0e2d6452ede64e14d
Dasha <smidariaa@gmail.com>
2019-11-18 12:41:00 +0300

    added registration check for the same email

--- js/start-page.js
+++ js/start-page.js
@@ -4,24 +4,28 @@ const loggedAsCollection = 'loggedAs';
 document.addEventListener('DOMContentLoaded', () => {
 
     // Сreate admin data for login in local
-    const usersArray = localStorage.getItem('usersArray');
+    const usersArray = localStorage.getItem(usersCollection);
     if (!usersArray) {
         let admin = [{email: 'admin@gmail.com', pass: 'root', isAdmin: true}];
         localStorage.setItem(usersCollection, JSON.stringify(admin));
     }
 
-    // User verification
+       // User verification
     let btnSignUp = document.querySelector('.btn-sign-up');
     btnSignUp.addEventListener('click', addUser);
 
     //User registration
     let btnSignIn = document.querySelector('.btn-sign-in');
     btnSignIn.addEventListener('click', verifySignIn);
+
+
 });
 
 
 function addUser() {
-    let users = JSON.parse(localStorage.getItem('usersArray')) || [];
+    let usersArrayLocalStorage = JSON.parse(localStorage.getItem(usersCollection)) ;
+    let emailError = document.querySelector('.email-error');
+
     let newUser = {
         fname: document.querySelector('.input-fname-sign-up').value,
         lname: document.querySelector('.input-lname-sign-up').value,
@@ -30,29 +34,30 @@ function addUser() {
         pass: document.querySelector('.input-pass-sign-up').value,
         isAdmin: false
     };
-    users.push(newUser);
-    localStorage.setItem(usersCollection, JSON.stringify(users));
+    if(usersArrayLocalStorage.find(item => item.email === newUser.email)){
+        emailError.classList.remove("d-none");
+    } else {
+        usersArrayLocalStorage.push(newUser);
+        localStorage.setItem(usersCollection, JSON.stringify(usersArrayLocalStorage));
+    }
 }
 
 
 function verifySignIn() {
     let email = document.querySelector('.input-email-sign-in').value;
     let pass = document.querySelector('.input-pass-sign-in').value;
-    let error = document.querySelector('.error');
+    let inputErrors = document.querySelector('.input-errors');
 
     let usersArrayLocalStorage = JSON.parse(localStorage.getItem(usersCollection));
-    if (usersArrayLocalStorage) {
-        const user = usersArrayLocalStorage.find(item => item.email === email && item.pass === pass);
+    const user = usersArrayLocalStorage.find(item => item.email === email && item.pass === pass);
         if (user) {
             localStorage.setItem(loggedAsCollection, JSON.stringify(user));
             document.location.href = user.isAdmin ? 'AdminPanel.html' : 'UserPanel.html';
         } else {
-            error.classList.remove("d-none");
+            inputErrors.classList.remove("d-none");
         }
-    } else {
-        error.classList.remove("d-none");
     }
-}
+
 
 
 
