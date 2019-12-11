edf99e8ad155c4444f137d658fafbb4c575b8d54
Dasha <smidariaa@gmail.com>
2019-12-11 10:47:11 +0300

    Added changes to the gitignore

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+import $ from 'jquery';
+import Popper from "popper.js";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './features/home/components/app/App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
